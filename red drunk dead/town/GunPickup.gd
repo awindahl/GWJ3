3ed4edd2cdf982168f8e498fc417a50e989dfc16
sumaris <alfred.radu@gmai.com>
2018-11-18 02:08:59 +0100

    added transition scene

--- red drunk dead/town/GunPickup.gd
+++ red drunk dead/town/GunPickup.gd
@@ -3,4 +3,5 @@ extends Spatial
 func _on_Area_body_entered(body):
 	if body.get("TYPE") == "PLAYER":
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		get_tree().change_scene("res://Saloon/WinScreen.tscn")
+		transition.fade_to("res://Saloon/WinScreen.tscn")
+		#get_tree().change_scene("res://Saloon/WinScreen.tscn")
